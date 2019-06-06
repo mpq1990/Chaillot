3eaa55649b1813ef05008e7846cce448ce0726cf
Majid Qureshi <mpq1990@gmail.com>
2019-06-06 18:25:20 +0200

    removing the routes for now

--- chaillot-client/src/App.vue
+++ chaillot-client/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
+<script>
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+export default {
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
